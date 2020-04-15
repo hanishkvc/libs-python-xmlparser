4910b4737ba0975eb66bbf0105e16355cf7a4548
hanishkvc <hanishkvc@gmail.com>
2020-04-15 13:56:30 +0530

    Find Start or End Tag, Add a simple Handler
    Use the Parser and Handler

--- xmlparser.py
+++ xmlparser.py
@@ -14,7 +14,11 @@ class XMLParser():
         hFile = open(sFile)
 
     def parse(self, handler):
+        TAGSTARTMARKERAGAIN = "TagStartMarkerAgain"
+        TAGENDMARKERALONE = "TagEndMarkerAlone"
         bTagMarkerStart = False
+        bTagTypeStart = True
+        iTagMarkerOffset = 0
         sCurTag = None
         for l in self.hFile:
             for c in l:
@@ -23,11 +27,41 @@ class XMLParser():
                         handler.error(l, self.TAGSTARTMARKERAGAIN)
                     else:
                         bTagMarkerStart = True
+                        bTagTypeStart = True
+                        iTagMarkerOffset = 0
                         sCurTag = c
-                if c == '>':
+                elif c == '>':
                     if bTagMarkerStart:
                         sCurTag += c
-                        handler.tag_start(l, sCurTag)
+                        if bTagTypeStart:
+                            handler.tag_start(l, sCurTag)
+                        else:
+                            handler.tag_end(l, sCurTag)
                         bTagMarkerStart = False
                     else:
                         handler.error(l, self.TAGENDMARKERALONE)
+                elif c == '/':
+                    if bTagMarkerStart:
+                        bTagTypeStart = False
+
+    def reset(self):
+        hFile.seek(0)
+
+class XMLParserHandler:
+    def error(sLine, errType):
+        print(errType)
+
+    def tag_start(sLine, sTag)
+        print(sTag)
+
+    def tag_end(sLine, sTag)
+        print("</{}>".format(sTag))
+
+
+myParser = XMLParser()
+myHandler = XMLParserHandler()
+myParser.open(sys.argv[1])
+myParser.parse(myHandler)
+
+
+
